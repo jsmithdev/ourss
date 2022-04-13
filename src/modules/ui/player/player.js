017eb8083904000657a488f55b2f30d6a2bd884f
Jamie Smith <me@jsmith.dev>
2022-04-13 00:26:21 -0400

    views, scroll, color, ux overhaul

--- src/modules/ui/player/player.js
+++ src/modules/ui/player/player.js
@@ -2,7 +2,7 @@ import { api, LightningElement } from 'lwc';
 
 export default class Player extends LightningElement {
 
-    autoplay = true;
+    autoplay = false;
     paused = true;
     currentTime = 0;
 
@@ -173,8 +173,6 @@ export default class Player extends LightningElement {
         this.dom.expand.classList.toggle('expand')
         this.dom.section.classList.toggle('expand')
         this.dom.section.classList.remove('expand-full')
-        this.showAdd = false
-        this.showSettings = false
     }
     renderedCallback() {
 
@@ -317,7 +315,18 @@ export default class Player extends LightningElement {
         }));
     }
 
-        
+    
+    handleTouchStart(event) {
+        if (this.ignoreSwipe(event)) {
+            this._xDown = undefined;
+            this._yDown = undefined;
+            return;
+        }
+    
+        const firstTouch = event.touches[0];
+        this._xDown = firstTouch.clientX;
+        this._yDown = firstTouch.clientY;
+    }
     handleTouchMove(event) {
 
         event.preventDefault();
@@ -336,19 +345,19 @@ export default class Player extends LightningElement {
             /*most significant*/
             if (this.xDiff > 0) {
                 /* left swipe */
-                console.info('app: left swipe ', true);
+                //console.info('app: left swipe ', true);
             } else {
                 /* right swipe */
-                console.info('app: right swipe ', true);
+                //console.info('app: right swipe ', true);
             }
         } else {
             if (this.yDiff > 0) {
                 /* up swipe */
-                console.info('app: up swipe ', true);
+                //console.info('app: up swipe ', true);
                 this.expand()
             } else {
                 /* down swipe */
-                console.info('app: down swipe ', true);
+                //console.info('app: down swipe ', true);
                 this.expand()
             }
         }
@@ -357,30 +366,10 @@ export default class Player extends LightningElement {
         this._xDown = null;
         this._yDown = null;
     }
-        
     ignoreSwipe(event) {
         // if some touches come from elements with ignoreswipe class > ignore
         return Array.from(event.touches).some((t) =>
             t.target.classList.contains('noswipe')
         );
-    }
-    
-    handleTouchStart(event) {
-        if (this.ignoreSwipe(event)) {
-            this._xDown = undefined;
-            this._yDown = undefined;
-            return;
-        }
-    
-        const firstTouch = event.touches[0];
-        this._xDown = firstTouch.clientX;
-        this._yDown = firstTouch.clientY;
-    }
-
-    isMobile() {
-        let check = false;
-        // eslint-disable-next-line no-useless-escape
-        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-        return check;
     }
 }
