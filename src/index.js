25acf4329734e3acbe3391010c4612436996b784
Jamie Smith <me@jsmith.dev>
2022-07-21 22:17:37 -0400

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { createElement } from 'lwc';
-import OurssApp from 'ourss/app';
-
-const app = createElement('ourss-app', { is: OurssApp });
-// eslint-disable-next-line @lwc/lwc/no-document-query
-document.querySelector('#main').appendChild(app);
